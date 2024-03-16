25f24d41fe66ca4c4152cad6951610b5858c5e6f
MirkoMoreno <mirkolucio@gmail.com>
2024-03-16 19:43:19 -0300

    he completado con informacion teorica y practica la materia de introduccion a los algoritmos

---
+++ pages/introalgoritmos/_meta.js
@@ -0,0 +1,7 @@
+export default { 
+    intro: "Introducción (IAA)",
+    validez_satisfacilidad: "Validez y Satisfacilidad",
+    logica_proposicional: "Lógica Proposicional",
+    funciones: "Funciones (IAA)",
+    induccion_calculo: "Inducción y Cálculo de Predicados",
+  }
