28fd3fc4b8aec7cc78d78581b30234dcd1f05d10
ignacionar <ignacionarvajadev@gmail.com>
2024-04-09 20:52:51 -0300

    Cambiar orden de páginas | MD1

--- pages/discreta1/_meta.js
+++ pages/discreta1/_meta.js
@@ -1,8 +1,8 @@
 export default { 
   intro: "Introducción (MD1)",
   numeros_enteros: "Números Enteros",
+  conteo: "Conteo",
   divisibilidad: "Divisibilidad",
   aritmetica_modular: "Aritmética Modular",
-  conteo: "Conteo",
   grafos: "Grafos"
 }
