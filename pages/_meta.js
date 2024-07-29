449d8e8386d63f707013413128597a85f2c340c8
ignacionar <ignacionarvajadev@gmail.com>
2024-07-29 02:20:50 -0300

    Añadir todo Intro. | IAA

--- pages/_meta.js
+++ pages/_meta.js
@@ -5,6 +5,7 @@ export default {
   acerca_del_proyecto: {
     title: <p>❓ Acerca del Proyecto</p>
   },
+  introalgoritmos: "Introducción a los Algoritmos",
   analisis1: "Análisis Matemático 1",
   discreta1: "Matemática Discreta 1",
   algoritmos1: "Algoritmos y Estructuras de Datos 1",
---
+++ pages/introalgoritmos/_meta.js
@@ -0,0 +1,7 @@
+export default { 
+  intro: "Introducción (IAA)",
+  validez_satisfacibilidad: "Validez y Satisfacibilidad",
+  logica_proposicional: "Lógica Proposicional",
+  funciones: "Funciones (IAA)",
+  induccion_calculo: "Inducción y Cálculo de Predicados",
+}
